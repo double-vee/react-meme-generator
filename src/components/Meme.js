a4fb5ffbde1661a6bc6be3299cd53071c53b37f6
Weronika Kansy <weronika.kansy@gmail.com>
2022-03-27 21:31:38 +0200

    Random meme object saved in state when the button has been clicked; form changed to div

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,23 +3,39 @@ import { useEffect, useState } from "react";
 export default function Meme() {
   const [memesArray, setMemesArray] = useState([]);
 
+  const [meme, setMeme] = useState({
+    name: "",
+    url: "",
+  });
+
   useEffect(() => {
     fetch("https://api.imgflip.com/get_memes")
       .then((response) => response.json())
       .then((result) => setMemesArray(result.data.memes));
   }, []);
 
+  function getMeme() {
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const { name, url } = memesArray[randomNumber];
+
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      name,
+      url,
+    }));
+  }
+
   return (
     <main className="meme">
-      <form className="meme__form">
+      <div className="meme__form">
         <div className="form__inputs">
           <input type="text" />
           <input type="text" />
         </div>
-        <button type="submit" className="form__btn">
+        <button onClick={getMeme} className="form__btn">
           Get a new meme image 🖼️
         </button>
-      </form>
+      </div>
     </main>
   );
 }
